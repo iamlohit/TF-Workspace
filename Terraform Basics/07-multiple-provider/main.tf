3cabc7599c6ff1b0b429689d5d58ebcd4ba16aae
Lohit <jethwanilohit@gmail.com>
2023-07-10 16:19:18 +0530

    Combining multiple providers in single tf script file

---
+++ Terraform Basics/07-multiple-provider/main.tf
@@ -0,0 +1,10 @@
+resource local_file file_res {
+    filename = "sample.txt"
+    content = "This is HCL"
+}
+
+resource random_string rstring {
+    length = 10
+}
+
+# Here we call the local_file and random_string are called Providers in terraform. 
