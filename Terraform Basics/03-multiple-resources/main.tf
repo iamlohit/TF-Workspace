0ef985c02fd87692b667a1e12a14907e68d312f5
Lohit <jethwanilohit@gmail.com>
2023-07-10 10:47:18 +0530

    Created multiple files from same tf script & modified file perms

--- Terraform Basics/03-multiple-resources/main.tf
+++ Terraform Basics/03-multiple-resources/main.tf
@@ -3,9 +3,13 @@
 resource local_file cat_res {
     filename = "cat.txt"
     content = "I adore cat's"
+    directory_permission = "0700"
+    file_permission      = "0700"
 }
 
 resource local_file dog_res {
     filename = "dog.txt"
     content = "I adore dog's"
+    directory_permission = "0700"
+    file_permission      = "0700"
 }
