54f09bcd447fc5069b1fcd1184e8e47d02bb2e5a
Lohit <jethwanilohit@gmail.com>
2023-07-10 16:58:31 +0530

    Testing implicit Dependency

---
+++ Terraform Basics/08-dependency/main.tf
@@ -0,0 +1,22 @@
+resource local_file file_res {
+    filename = "implicit.txt"
+    content = "This is random String from RP : ${random_string.rstring.id}"
+    depends_on = [ random_string.rstring ]
+}
+
+resource random_string rstring {
+    length = 10
+}
+
+# This is a demo of an IMPLICIT dependency.
+# Here we call the value in "id" from the random_string provider inside the local_file provider.
+# Note: that HCL implicitly finds the dependency that to put the id value in local_file content it needs to run the random_string provider first.
+
+# Terraform Apply output:
+    # random_string.rstring: Creating...
+    # random_string.rstring: Creation complete after 0s [id=0sNii[L#:g]
+    # local_file.file_res: Creating...
+    # local_file.file_res: Creation complete after 0s [id=0ea78203fc5ee3dc82fe721852f07157183722e7]
+
+# Explicit Dependency:
+    # Here we explicity define the dependency with the "depends-on" argument in the local_file provider.
