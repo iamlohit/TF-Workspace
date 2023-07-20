cf166092d9722b27265a7c5590648db10ed8214c
Lohit <jethwanilohit@gmail.com>
2023-07-20 14:40:47 +0530

    Attached additional disk to vm

--- GCP/05-google-compute-engine/main.tf
+++ GCP/05-google-compute-engine/main.tf
@@ -24,6 +24,13 @@ resource "google_compute_instance" "vm-from-tf" {
       preemptible = true
       automatic_restart = false
     }
+
+    # Note: When using google_compute_attached_disk you must use lifecycle.ignore_changes = ["attached_disk"] on the google_compute_instance resource that has the disks attached.
+    # Otherwise the two resources will fight for control of the attached disk block.
+
+    lifecycle {
+      ignore_changes = [ attached_disk ]
+    }
 }
 
 # Adding persistant disk, so it remains even if VM is deleted.
@@ -32,4 +39,11 @@ resource "google_compute_disk" "tf-pd-ssd-1" {
     name = "tf-pd-ssd-1"
     type = "pd-ssd"
     zone = "us-central1-a"
+}
+
+# Now attaching the disk to the machine
+
+resource "google_compute_attached_disk" "default" {
+  disk = google_compute_disk.tf-pd-ssd-1.id
+  instance = google_compute_instance.vm-from-tf.id
 }
