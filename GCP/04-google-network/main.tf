15b50c107907bcd18a41c6875fcc563f10b92edb
Lohit <jethwanilohit@gmail.com>
2023-07-20 07:36:23 +0530

    Created subnet within Customer VPC

--- GCP/04-google-network/main.tf
+++ GCP/04-google-network/main.tf
@@ -8,6 +8,13 @@ resource google_compute_network custom-vpc-tf {
     auto_create_subnetworks = false
 }
 
+resource "google_compute_subnetwork" "usc1-subnet" {
+    name = "usc1-subnet"
+    network = google_compute_network.custom-vpc-tf.id
+    ip_cidr_range = "10.1.0.0/24"
+    region = "us-central1"
+}
+
 output "auto-vpc" {
   value = google_compute_network.auto-vpc-tf
 }
