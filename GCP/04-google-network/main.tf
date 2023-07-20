e8f674c90314914f12a4048e4f3db20e20e93d10
Lohit <jethwanilohit@gmail.com>
2023-07-20 07:26:52 +0530

    Created GCP 2 VPC's

--- GCP/04-google-network/main.tf
+++ GCP/04-google-network/main.tf
@@ -4,6 +4,14 @@ resource "google_compute_network" "auto-vpc-tf" {
 }
 
 resource google_compute_network custom-vpc-tf {
-    name = "auto-vpc-tf"
+    name = "custom-vpc-tf"
     auto_create_subnetworks = false
+}
+
+output "auto-vpc" {
+  value = google_compute_network.auto-vpc-tf
+}
+
+output "custom" {
+  value = google_compute_network.custom-vpc-tf
 }
