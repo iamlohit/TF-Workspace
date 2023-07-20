ef41cd08bf5334f2441821009f19b559afe762e8
Lohit <jethwanilohit@gmail.com>
2023-07-20 06:53:37 +0530

    Added lifecycle & retention period

--- GCP/03-google-cloud-storage/main.tf
+++ GCP/03-google-cloud-storage/main.tf
@@ -9,13 +9,29 @@ resource "google_storage_bucket" "GCS1" {
 
   labels = {
     "env" = "dev"
-    "retailer" = "Krogers"
+    "retailer" = "krogers"
   }
+
+  lifecycle_rule {
+    condition {
+      age = 2
+    }
+    action {
+      type = "SetStorageClass"
+      storage_class = "COLDLINE"
+    }
+  }
+  retention_policy {
+    is_locked = true
+    retention_period = 864000
+  }
+
 }
+
+  
 
 resource "google_storage_bucket_object" "picture" {
   name = "abstract_wallpaper"
   bucket = google_storage_bucket.GCS1.name
   source = "architecture-building-city-glass-wallpaper-preview.jpg"
 }
-
