d6967e082aed0d2e6d612866ec6097c601c4b966
Lohit <jethwanilohit@gmail.com>
2023-07-23 14:22:56 +0530

    Added PubSub 101 explanation

--- GCP/09-Google-cloud-PubSub/main.tf
+++ GCP/09-Google-cloud-PubSub/main.tf
@@ -3,4 +3,8 @@
 #     It works by creating a Publisher Topic.
 #     The Subscriber subscribes to the Topic.
 #     Whenever we want to share information with the subscriber, we just add it to Topic and Subscribers recieve it.
+#     There are two types of delivery of this message:
+#       A. Pull - This means the subscriber has to manually pull for any new messages from the Publisher topic.
+#       B. Push - This means the Publisher topic will automatically push any new messages to a Subscriber's endpoint URL.
+#     These messages can be retained for upto 7 days, but it depends on the implementation/platform.
 
