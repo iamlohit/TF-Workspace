889dfa6d08cd6f40c593a893e0f84a9965d84866
Lohit <jethwanilohit@gmail.com>
2023-07-23 14:25:02 +0530

    Added PubSub 101 explanation

--- GCP/09-Google-cloud-PubSub/main.tf
+++ GCP/09-Google-cloud-PubSub/main.tf
@@ -7,4 +7,6 @@
 #       A. Pull - This means the subscriber has to manually pull for any new messages from the Publisher topic.
 #       B. Push - This means the Publisher topic will automatically push any new messages to a Subscriber's endpoint URL.
 #     These messages can be retained for upto 7 days, but it depends on the implementation/platform.
+#     Schemas: The message can be formatted to a specific schema that needs to be ahered by the publisher like json.
+#       Without this, we can have free form messages as well, but that is rarely useful.
 
