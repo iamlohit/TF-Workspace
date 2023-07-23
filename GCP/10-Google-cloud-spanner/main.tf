348cb89eb6e0dd9f3983afaa9597d2b2411be26f
Lohit <jethwanilohit@gmail.com>
2023-07-23 17:57:35 +0530

    Created DB in Spanner instance & 2 Tables in DB using standard SQL Lang

--- GCP/10-Google-cloud-spanner/main.tf
+++ GCP/10-Google-cloud-spanner/main.tf
@@ -12,3 +12,15 @@ resource "google_spanner_instance" "spanner_tf" {
     "env" = "learningtf" 
   }
 }
+
+# Creating Database now
+
+resource "google_spanner_database" "spanner_db_tf" {
+  instance = google_spanner_instance.spanner_tf.name
+  name = "spanner_db_tf"
+  version_retention_period = "3d"
+  ddl = [
+    "CREATE TABLE t1 (t1 INT64 NOT NULL,) PRIMARY KEY(t1)",
+    "CREATE TABLE t2 (t2 INT64 NOT NULL,) PRIMARY KEY(t2)",
+  ]
+}
