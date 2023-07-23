2429ad4b8cf4d8891dbdf45e50a064807d105b6c
Lohit <jethwanilohit@gmail.com>
2023-07-23 07:59:08 +0530

    Added the function call, runtime env, storage location, & service configuration with URL output

--- GCP/07-Google-cloud-functions/main.tf
+++ GCP/07-Google-cloud-functions/main.tf
@@ -10,3 +10,29 @@ resource "google_storage_bucket_object" "object" {
   source = "index.js"
 }
 
+resource "google_cloudfunctions2_function" "function" {
+  name = "function-v2-tf"
+  location = "us-central1"
+  description = "A new V2 GCF using TF"
+
+  build_config {
+    runtime = "nodejs16"
+    entry_point = "helloWorldtf"
+    source {
+      storage_source {
+        bucket = google_storage_bucket.bucket.name
+        object = google_storage_bucket_object.object.name
+      }
+    }
+  }
+  service_config {
+    max_instance_count = 5
+    available_memory = "256M"
+    timeout_seconds = 300
+  }
+}
+
+output "function_uri" { 
+  value = google_cloudfunctions2_function.function.service_config[0].uri
+}
+
