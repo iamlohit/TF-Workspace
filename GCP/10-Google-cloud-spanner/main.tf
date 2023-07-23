d95d7b54adfeab5bbc995a8fa65e6c4693318e5e
Lohit <jethwanilohit@gmail.com>
2023-07-23 17:53:54 +0530

    Updated Spanner 101

--- GCP/10-Google-cloud-spanner/main.tf
+++ GCP/10-Google-cloud-spanner/main.tf
@@ -1,10 +1,11 @@
 # Spanner 101:
 #     This is highly available (99.99% availability) and distributed (min. 3 read/write replicas) and horizontally scalable fast SQL database.
 #     Here we are going to create a Spanner instance below.
+#     Inside the instance, you can create a database.
 
 resource "google_spanner_instance" "spanner_tf" {
   name = "spannertf"
-  config = "regional-asia-south1"
+  config = "regional-asia-south2"
   display_name = "Spanner from TF"
   num_nodes = 1
   labels = {
