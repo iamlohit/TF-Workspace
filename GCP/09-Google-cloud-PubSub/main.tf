a676d82de173d221a523da0977e57138739ac0c6
Lohit <jethwanilohit@gmail.com>
2023-07-23 13:38:44 +0530

    Added PubSub 101 explanation

---
+++ GCP/09-Google-cloud-PubSub/main.tf
@@ -0,0 +1,6 @@
+# PubSub 101:
+#     When you want to decouple two systems but enable asynchronous communication between those systems.
+#     It works by creating a Publisher Topic.
+#     The Subscriber subscribes to the Topic.
+#     Whenever we want to share information with the subscriber, we just add it to Topic and Subscribers recieve it.
+
