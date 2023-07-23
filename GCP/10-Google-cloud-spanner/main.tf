a159481e6eb2328677b1a9f7161c7460b1614051
Lohit <jethwanilohit@gmail.com>
2023-07-23 17:48:24 +0530

    Created a single spanner instance

--- GCP/10-Google-cloud-spanner/main.tf
+++ GCP/10-Google-cloud-spanner/main.tf
@@ -3,7 +3,7 @@
 #     Here we are going to create a Spanner instance below.
 
 resource "google_spanner_instance" "spanner_tf" {
-  name = "spanner_tf"
+  name = "spannertf"
   config = "regional-asia-south1"
   display_name = "Spanner from TF"
   num_nodes = 1
